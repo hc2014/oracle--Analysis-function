b708106c45acaac87996448a996b2676e0e0aa7c
hc2014 <294836881@qq.com>
2015-11-13 13:53:26 +0800

    a

--- 分析函数.sql
+++ 分析函数.sql
@@ -20,7 +20,7 @@ drop table hc_testc
 --row_number() over()
 select y,v,sal, row_number() over(partition by y order by v) from hc_testc
 --sum() over()
-select y,v,sal,sum(sal) over(partition by y order by v) from hc_testc
+select y,v,sal,sum(sal) over(partition by y order by v ) from hc_testc
 
 --Lag和Lead分析函数可以在同一次查询中取出同一字段的前N行的数据(Lag)和后N行的数据(Lead)作为独立的列。
 --lag三个参数(字段名,偏移量，默认值) 如果over()用到了partiton by分钟，那么前后偏移的都是只能是在分组内的
@@ -30,6 +30,15 @@ select y,v,sal,first_value(sal) over(partition by y order by v) fValue,
 last_value(sal) over(partition by y order by v) lValue
  from hc_testc
 
-
-
+--rownumber,rank,dense_rank
+select y,v,row_number() over(order by y) rownumber,
+rank() over(order by y) rank,
+dense_rank() over(order by y) dense
+ from hc_testc
+ 
+ --cume_dist、percent_rank
+ select y,v,sal,rank() over(order by sal) rank,
+ cume_dist() over(order by sal) cume,
+ percent_rank() over(order by sal) perc--相对位置-1/总行数-1
+  from hc_testc
 
